0a21cadc74ecbb3ba02927887a3da95993258b75
Usolcev Vladimir <you@example.com>
2014-10-19 15:45:01 +0700

    PROCESS

--- sites/all/themes/tours_theme/js/tours_theme.js
+++ sites/all/themes/tours_theme/js/tours_theme.js
@@ -3,16 +3,19 @@
     attach: $(function () {
       $("#edit-country-to").change(function () {
         var regionSelect = $('select[name="region"]');
-        var countrySelect = $('select[name="country_to"]')
+        var countrySelect = $('select[name="country_to"]');
+        var citySelect = $('select[name="city"]');
         //alert(countrySelect.val());
         $.ajax({
           url: '/get_region',
           type: 'POST',
           data: {
-            country_to: countrySelect.val()
+            country_to: countrySelect.val(),
+            city: citySelect.val()
           },
           success: function(response) {
-            alert(response);
+            regionSelect.html('');
+            regionSelect.append(response);
           },
           error: function(response) {
             alert('false');
